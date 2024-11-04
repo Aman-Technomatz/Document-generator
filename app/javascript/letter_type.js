a3b31fe7fe20616a84fc996ebb44fe713daefafe
Aman Tomar <atomar@technomatz.com>
2024-11-04 19:53:00 +0530

    fix javascript code

--- app/javascript/letter_type.js
+++ app/javascript/letter_type.js
@@ -4,28 +4,24 @@ document.addEventListener('DOMContentLoaded', function() {
   const experienceLetterFields = document.getElementById('experience_letter_fields');
   const resignationLetterFields = document.getElementById('resignation_letter_fields');
   const incrementLetterFields = document.getElementById('increment_letter_fields');
-  const applicationLetterFields = document.getElementById('application_letter_fields');
+
   function toggleFields() {
-    const documentType = documentTypeSelect.value;
-    offerLetterFields.style.display = 'none';
-    experienceLetterFields.style.display = 'none';
-    resignationLetterFields.style.display = 'none';
-    applicationLetterFields.style.display = 'none';
-    incrementLetterFields.style.display = 'none';
-    if (documentType === "experience_letter") {
-      experienceLetterFields.style.display = 'block';
-    } else if (documentType === "Offer Letter") {
-      offerLetterFields.style.display = 'block';
-    }
-    else if (documentType === "Resign Letter") {
-      resignationLetterFields.style.display = 'block';
-    }
-    else if (documentType === "Increment Letter") {
-      incrementLetterFields.style.display = 'block';
-    }
-    else if (documentType === "Application Letter") {
-      applicationLetterFields.style.display = 'block';
-    }
+      const documentType = documentTypeSelect.value;
+
+      offerLetterFields.style.display = 'none';
+      experienceLetterFields.style.display = 'none';
+      resignationLetterFields.style.display = 'none';
+      incrementLetterFields.style.display = 'none';
+
+      if (documentType === "experience_letter") {
+          experienceLetterFields.style.display = 'block';
+      } else if (documentType === "offer_letter") {
+          offerLetterFields.style.display = 'block';
+      } else if (documentType === "resign_letter") {
+          resignationLetterFields.style.display = 'block';
+      } else if (documentType === "increment_letter") {
+          incrementLetterFields.style.display = 'block';
+      }
   }
 
   documentTypeSelect.addEventListener('change', toggleFields);
