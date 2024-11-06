9f3fcaa20e67092f5b5470707c966bac48199048
Sangeeta dhiman <sdhiman@technomatz.com>
2024-11-06 18:42:57 +0530

    Added pagination or search bar

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,12 @@
 class Document < ApplicationRecord
   belongs_to :user, optional: true
   accepts_nested_attributes_for :user
+
+  scope :search_by_query, ->(query) {
+    joins(:user).where(
+      "documents.id LIKE :query OR documents.document_type LIKE :query OR documents.employee_id LIKE :query OR users.name LIKE :query OR users.id LIKE :query",
+      query: "%#{query}%"
+    )
+  }
+
 end
