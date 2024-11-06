b870125ab18b62cea5aea35b083e127a34fb3545
Sangeeta dhiman <sdhiman@technomatz.com>
2024-11-06 13:23:58 +0530

    fix deletetion issue

--- app/javascript/form_toggle.js
+++ app/javascript/form_toggle.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
+  const deleteButtons = document.querySelectorAll('.delete-button');
+  deleteButtons.forEach(function(deleteButton) {
+    deleteButton.addEventListener('click', function(event) {
+      const confirmed = window.confirm('Are you sure?');
+      if (!confirmed) {
+        event.preventDefault();
+      }
+    });
+  });
+
   function toggleUserFields(showNewUserFields) {
     const newUserFields = document.getElementById('new_user_fields');
     const userSelect = document.getElementById('document_user_id');
